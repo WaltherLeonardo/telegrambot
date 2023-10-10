65bf2b5c70c92e919d8e91f4bc27b7c245902f6b
walther <waltherleonardo@hotmail.com>
2023-10-10 07:53:05 -0500

    attemps to open in memory

--- cloud bot/main.py
+++ cloud bot/main.py
@@ -1,5 +1,7 @@
 import asyncio
 import io
+import os
+import json
 
 import nest_asyncio
 
@@ -8,13 +10,18 @@ from telegram import Update
 from telegram.ext import filters, Application, CommandHandler, MessageHandler, ContextTypes
 
 from office365.runtime.auth.user_credential import UserCredential
-# from office365.runtime.http.request_options import RequestOptions
+from office365.runtime.http.request_options import RequestOptions
 from office365.sharepoint.client_context import ClientContext
 from office365.sharepoint.files import file
 
 nest_asyncio.apply()
 
-SITEURL="https://panamotorssac-my.sharepoint.com/personal/pyanaura_panaautos_com_pe/Documents/"
+# from openpyxl import load_workbook
+
+import pandas as pd
+
+SITEURL="https://panamotorssac-my.sharepoint.com/personal/pyanaura_panaautos_com_pe"
+FILE_URL="/Documents/Reporte%20%20Modelo%20Retail%20&%20Wholesale%20de%20Ventas%202022.xlsx"
 PASSWORD="Panaautos2023"
 USERNAME="wrojas@panaautos.com.pe"
 TOKEN="6528642672:AAEBweExhh5x5jZCkQEzVkrRyi6k6gIc1Ic"
@@ -23,20 +30,45 @@ TOKEN="6528642672:AAEBweExhh5x5jZCkQEzVkrRyi6k6gIc1Ic"
 # logging.getLogger("httpx").setLevel(logging.WARNING)
 '''core Sharepoint Functions'''
 ''' ------------------------------------------------------------------------------------------------------------------'''
-# sharepoint core loging
+# sharepoint core login
 async def loging():
     username=USERNAME
     password=PASSWORD
     site_url=SITEURL
+    file_url=FILE_URL
     ctx = ClientContext(site_url).with_credentials(UserCredential(f"{username}", f"{password}"))
+    
+    request = RequestOptions("{0}/_api/web/".format(site_url))
+    response = ctx.pending_request().execute_request_direct(request)
+    ajson = json.loads(response.content)
+    web_title = ajson['d']['Title']
+    print(web_title)
+
     ctx.load(ctx.web)
     ctx.execute_query()
-    file_url="Reporte%20%20Modelo%20Retail%20&%20Wholesale%20de%20Ventas%202022.xlsx"
+
+    # response = file.File.open_binary(ctx, file_url)   
+    # response = ctx.web.get_file_by_server_relative_url(file_url).open_binary(ctx, file_url)
+    # response = ctx.web.get_file_by_server_relative_url(file_url)
+    # with open(file=file_url ,mode="rb"):
+    # response = ctx.web.get_file_by_guest_url(file_url).open_binary(ctx, file_url)
+    # response = file.File.open_binary(ctx, file_url)
+    # # bytes_file_obj=io.BytesIO()
+    # # bytes_file_obj.write(response.content)
+    # # bytes_file_obj.seek(0)
+    # bytes_file_obj=io.BytesIO(response.content)
+    # bytes_file_obj.seek(0)
+    # print(type(bytes_file_obj))
+    # wb =  load_workbook(bytes_file_obj)
+    # df = pd.read_excel(bytes_file_obj)
+
     response = file.File.open_binary(ctx, file_url)
-    bytes_file_obj=io.BytesIO()
+    bytes_file_obj = io.BytesIO()
     bytes_file_obj.write(response.content)
-    bytes_file_obj.seek(0)
-    return bytes_file_obj
+    bytes_file_obj.seek(0) #set file object to start
+    df = pd.read_excel(bytes_file_obj.read(), engine='openpyxl')
+
+    return df
 ''' ------------------------------------------------------------------------------------------------------------------'''
 
 
@@ -62,7 +94,7 @@ async def log():
 application = Application.builder().token(TOKEN).build()
 
 async def ventasSI(update:Update, context:ContextTypes.DEFAULT_TYPE ):
-    fileobject = await asyncio.run(log())
+    fileobject = asyncio.run(log())
     # asyncio.run(down(contexto))
     print(' - '*10)
     print(type(fileobject))
